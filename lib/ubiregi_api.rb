e595ea4cdc7baaca33819bcdf1acb852420cf27f
Takayuki Hirano <hirano@ubiregi.com>
2015-05-26 15:03:45 +0900

    client

--- lib/ubiregi_api.rb
+++ lib/ubiregi_api.rb
@@ -5,8 +5,8 @@ require "optparse"
 require 'simple_uuid'
 require 'time'
 
-require "ubiregi_api/version"
-require "ubiregi_api/client.rb"
+require "./lib/ubiregi_api/version"
+require "./lib/ubiregi_api/client.rb"
 
 module UbiregiAPI
 end
